fe03ccedb51e75ef8db66cee99223ab54876fb28
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-11-30 12:29:22 +0330

    feat: develop addTodo function

--- JS/Main.js
+++ JS/Main.js
@@ -8,3 +8,25 @@ const AddNewTaskBtn = $.querySelector('.AddNewTaskBtn')
 const AddNewTodoWrapper = $.querySelector('#AddNewTodoWrapper')
 const ListItems = $.querySelectorAll('.list-items')
 const Todos = $.querySelectorAll('.Todos')
+
+let setId = 1;
+
+
+function addTodo(){
+  let NewLi = $.createElement('li');
+  let NewImg = $.createElement('img');
+  NewLi.innerHTML = AddNewTaskInput.value;
+  NewLi.setAttribute('draggable' , true);
+  NewLi.setAttribute('id' , setId);
+  NewLi.setAttribute('class' , 'Todos');
+  NewImg.setAttribute('src' , '../Assets/Images/Trash.svg');
+  NewImg.setAttribute('class' , 'TrashIcon');
+  NewLi.appendChild(NewImg);
+  NewLi.setAttribute('class' , 'Todos');
+  AddNewTodoWrapper.appendChild(NewLi);
+  NewLi.addEventListener('dragstart' , dragstartHandler);
+  NewImg.addEventListener('click' , deleteTodoHandler);
+  CloseModal();
+  AddNewTaskInput.value = "";
+  setId++;
+}
